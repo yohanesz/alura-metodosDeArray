f7b63eac75906ebfc360bab91740c4adfa51338e
yohanês <yzanghelini@gmail.com>
2025-04-26 18:02:22 -0300

    atualizando repositório

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -5,7 +5,7 @@ buttons.forEach(btn => btn.addEventListener('click', filterBooks))
 function filterBooks() {
     const btnElement = document.getElementById(this.id);
     const category = btnElement.value;
-    let filteredBooks = books.filter(book => book.categoria == category);
+    let filteredBooks = category == "disponivel" ? books.filter(book => book.quantidade > 0) : books.filter(book => book.categoria == category);
 
     showBooks(filteredBooks);
 }
